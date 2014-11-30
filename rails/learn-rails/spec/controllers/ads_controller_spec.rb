f8f531056275f48dd1d23d8f52da4bfc28e47f84
Vineeth B S <vineeth@c42.in>
2014-11-30 20:57:10 +0530

    adding index action with the specs

--- rails/learn-rails/spec/controllers/ads_controller_spec.rb
+++ rails/learn-rails/spec/controllers/ads_controller_spec.rb
@@ -3,10 +3,24 @@ require 'rails_helper'
 RSpec.describe AdsController, :type => :controller do
   describe "show" do
     it "should fetch the details about the ad for the ad id passed" do
-      ad = Ad.create(name:"name", description: "description", price: 20,seller_id: 10, email: "user@example.com", img_url: "url")
+      ad = Ad.create(name: "name", description: "description", price: 20, seller_id: 10, email: "user@example.com", img_url: "url")
       get :show, {id: ad.id}
       expect(response).to render_template("show")
-      expect(assigns(:ad)).to eq(Ad.find(ad.id))
+      expect(assigns(:ad)).to eq(Ad.last)
+    end
+  end
+
+  describe "index" do
+    it "should list all the ads in the system" do
+      10.times do |index|
+        Ad.create(name: "name_#{index}", description: "description", price: 20, seller_id: 10, email: "user@example.com", img_url: "url")
+      end
+
+      get :index
+
+      expect(response).to render_template("index")
+
+      expect(assigns(:ads)).to eq(Ad.all)
     end
   end
 end
